87725c189930255abdc546559c94c213eac6416f
Le Ngoc Long <alphahoai@gmail.com>
2019-04-26 14:24:54 +0700

    Update MyExtensions.swift

--- DemoChat/MyExtensions.swift
+++ DemoChat/MyExtensions.swift
@@ -134,13 +134,33 @@ extension UITableView {
     }
 }
 
-extension String {
-    func indexOf(_ input: String,
-                 options: String.CompareOptions = .literal) -> String.Index? {
-        return self.range(of: input, options: options)?.lowerBound
+extension StringProtocol { // for Swift 4.x syntax you will needed also to constrain the collection Index to String Index - `extension StringProtocol where Index == String.Index`
+    func index(of string: Self, options: String.CompareOptions = []) -> Index? {
+        return range(of: string, options: options)?.lowerBound
     }
-
-    func lastIndexOf(_ input: String) -> String.Index? {
-        return indexOf(input, options: .backwards)
+    func endIndex(of string: Self, options: String.CompareOptions = []) -> Index? {
+        return range(of: string, options: options)?.upperBound
+    }
+    func indexes(of string: Self, options: String.CompareOptions = []) -> [Index] {
+        var result: [Index] = []
+        var startIndex = self.startIndex
+        while startIndex < endIndex,
+            let range = self[startIndex...].range(of: string, options: options) {
+                result.append(range.lowerBound)
+                startIndex = range.lowerBound < range.upperBound ? range.upperBound :
+                    index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
+        }
+        return result
+    }
+    func ranges(of string: Self, options: String.CompareOptions = []) -> [Range<Index>] {
+        var result: [Range<Index>] = []
+        var startIndex = self.startIndex
+        while startIndex < endIndex,
+            let range = self[startIndex...].range(of: string, options: options) {
+                result.append(range)
+                startIndex = range.lowerBound < range.upperBound ? range.upperBound :
+                    index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
+        }
+        return result
     }
 }
