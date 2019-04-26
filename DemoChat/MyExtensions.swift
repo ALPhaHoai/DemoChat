bb5cb83bf57ea9e98ada4354af1c2c5e48cf3042
Le Ngoc Long <alphahoai@gmail.com>
2019-04-26 13:52:21 +0700

    Update MyExtensions.swift

--- DemoChat/MyExtensions.swift
+++ DemoChat/MyExtensions.swift
@@ -134,3 +134,13 @@ extension UITableView {
     }
 }
 
+extension String {
+    func indexOf(_ input: String,
+                 options: String.CompareOptions = .literal) -> String.Index? {
+        return self.range(of: input, options: options)?.lowerBound
+    }
+
+    func lastIndexOf(_ input: String) -> String.Index? {
+        return indexOf(input, options: .backwards)
+    }
+}
